cb0b7d1a02cba85614b7bcbb8fc3745f89910be3
icarodante <icaro.menezes@sptech.school>
2022-09-21 11:35:20 -0300

    adição cadastro e login no angular

---
+++ client-side/src/app/components/arf-cadastro/arf-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arf-cadastro',
+  templateUrl: './arf-cadastro.component.html',
+  styleUrls: ['./arf-cadastro.component.scss']
+})
+export class ArfCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client-side/src/app/components/arf-login/arf-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arf-login',
+  templateUrl: './arf-login.component.html',
+  styleUrls: ['./arf-login.component.scss']
+})
+export class ArfLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
