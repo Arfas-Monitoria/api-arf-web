e8b6366250bd216e9b5b2cc85c764e840006c343
icarodante <icaro.menezes@sptech.school>
2022-09-26 22:50:43 -0300

    fix: canva e btn login/cadastro

--- client-side/src/app/components/arf-cadastro/arf-cadastro.component.ts
+++ client-side/src/app/components/arf-cadastro/arf-cadastro.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-arf-cadastro',
   templateUrl: './arf-cadastro.component.html',
-  styleUrls: ['./arf-cadastro.component.scss']
+  styleUrls: ['./arf-cadastro.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ArfCadastroComponent implements OnInit {
 
--- client-side/src/app/components/arf-login/arf-login.component.ts
+++ client-side/src/app/components/arf-login/arf-login.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-arf-login',
   templateUrl: './arf-login.component.html',
-  styleUrls: ['./arf-login.component.scss']
+  styleUrls: ['./arf-login.component.scss',],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ArfLoginComponent implements OnInit {
 
