52c10270e68ab9a6380dfd3ea71f0f9471881756
Romvinicius <vinicius.roman@sptech.school>
2022-10-01 20:05:13 -0300

    feature: Criando serviços da API

---
+++ client-side/src/app/interfaces/usuarios.ts
@@ -0,0 +1,22 @@
+export interface Icadastro{
+  email: string,
+  senha: string
+}
+
+export interface Ilogin{
+  email: string,
+  senha: string
+}
+
+// export interface IdadosUsuario{
+//   registro?: string,
+//   nomeFuncionario?: string, 
+//   usuario?: string,  
+//   email?: string, 
+//   telefone?: string, 
+//   funcao?: string, 
+//   senha?: string, 
+//   permissaoDashboard?: string, 
+//   status?: string, 
+//   fkDepartamento?: string
+// }
