50d60028c08408ab743358d083104f0e28c3db5e
icarodante <icaro.menezes@sptech.school>
2022-11-23 22:06:22 -0300

    tela de alterar dados quase pronta + nav

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ArfCadastroComponent } from './components/arf-cadastro/arf-cadastro.component';
 import { ARFHomepageComponent } from './components/arf-homepage/arf-homepage.component';
 import { ArfLoginComponent } from './components/arf-login/arf-login.component';
+import { ArfAlterarDadosComponent } from './components/arf-oversight-page/arf-alterar-dados/arf-alterar-dados.component';
 import { ArfDashboardPageComponent } from './components/arf-oversight-page/arf-dashboard/arf-dashboard-page.component';
 import { ArfOversightPageComponent } from './components/arf-oversight-page/arf-oversight-page.component';
 
@@ -14,6 +15,7 @@ const routes: Routes = [
     path: 'oversight', component: ArfOversightPageComponent,
     children: [
       { path: 'dashboard', component: ArfDashboardPageComponent },
+      { path: 'alterar-dados', component: ArfAlterarDadosComponent },
     ]
   },
 ];
