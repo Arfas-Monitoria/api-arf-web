7deb96c6fea950158647a0141faf289e175a8ba0
icarodante <icaro.menezes@sptech.school>
2022-11-23 22:47:12 -0300

    adição alterar dados

---
+++ client-side/src/app/components/arf-oversight-page/arf-alterar-dados/arf-alterar-dados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arf-alterar-dados',
+  templateUrl: './arf-alterar-dados.component.html',
+  styleUrls: ['./arf-alterar-dados.component.scss']
+})
+export class ArfAlterarDadosComponent implements OnInit {
+  error: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
